72f85a7fe73a8b8e87fcb4a89ab948f0fdd86a05
Ugarte-George <grgugarte@comcast.net>
2021-04-13 20:05:57 -0400

    testing

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,8 +1,8 @@
 import $ from "jquery";
 import {gsap} from "gsap";
 
-var redBox = $('.red-box');
 
+var redBox = $('.red-box');
 
 // only here to make codeKit be quiet!
 console.log(gsap);
@@ -10,6 +10,6 @@ $(document).ready(function(){
 
     console.log ('Page Loaded');
 
-    gsap.to(redBox , {duration:  0.5, x:300});
+    gsap.from(redBox , {duration:  0.5, x:300});
 
 });
